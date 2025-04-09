8453a2b72c14ea79a658e491a606aacd3587f32a
Blank <blank@BlankPC.localdomain>
2025-04-09 19:14:16 +0200

    Initial Janitor

--- janitor/janitor.c
+++ janitor/janitor.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 
 int filter (const struct dirent *name){
-    return 1;
+    const char *extension = strrchr(name->d_name, '.');
+    if (extension != NULL) {
+        return (0 == strcmp(extension, ".mp3"));
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -20,37 +25,28 @@ int main(int argc, char *argv[]) {
         perror("Scandir");
         exit(EXIT_FAILURE);
     }
+    int pathLength = strlen(argv[1]);
 
-    while (n--){
-        printf("NAME: %s\n------------------------\n", namelist[n]->d_name);
-        switch (namelist[n]->d_type) {
-            case DT_UNKNOWN:
-                puts("TYPE: unknown");
-                break;
-            case DT_FIFO:
-                puts("TYPE: fifo");
-                break;
-            case DT_CHR:free(namelist);
-                puts("TYPE: directory");
-                break;
-            case DT_BLK:
-                puts("TYPE: block device");
-                break;
-            case DT_REG:
-                puts("TYPE: regular");
-                break;
-            case DT_LNK:
-                puts("TYPE: link");
-                break;
-            case DT_SOCK:
-                puts("TYPE: unix domain socket");
-                break;
-            case DT_WHT:
-                puts("TYPE: whiteout");
-                break;
+    for (int i = 0; i < n; i++) {
+        char *fullpath = malloc(pathLength + 1 + strlen(namelist[i]->d_name) + 1);
+        if (fullpath == NULL) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
         }
-        free(namelist[n]);
+
+        strcpy(fullpath, argv[1]);
+
+        if (argv[1][pathLength-1] != '/') {
+            strcat(fullpath, "/");
+        }
+
+        strcat(fullpath, namelist[i]->d_name);
+        remove(fullpath);
+
+        free(fullpath);
+        free(namelist[i]);
     }
+
     free(namelist);
     
     exit(EXIT_SUCCESS);
